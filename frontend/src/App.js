2b955b9a7addbcaa85a7588216426d67c857fbc9
Mounika <mounikakattubadi94@gmail.com>
2025-07-04 15:24:00 +0530

    aboutpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+
 function App() {
   return (
     <Router>
       
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
       </Routes>
     </Router>
   );
