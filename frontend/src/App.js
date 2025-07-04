c8a58abd7e209ad1e8a9bf928cfb9b02012aa386
GannuNb <nbganesh2002@gmail.com>
2025-07-04 15:40:18 +0530

    WIP: Contact page changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Contactus from './Contactus';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contactus/>} />
       </Routes>
     </Router>
   );
