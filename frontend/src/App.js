efe5ba768c0a146c855d54b3b1dfd7b571928bb0
Mounika <mounikakattubadi94@gmail.com>
2025-07-04 16:19:40 +0530

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,9 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contactus from './Contactus';
-
 import Enquire from './Enquire';
-
 import Products from './Products';
-
+import Footer from './Footer';
 
 function App() {
   return (
@@ -15,14 +13,11 @@ function App() {
       
       <Routes>
         <Route exact path="/" element={<Home />} />
-
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<Contactus/>} />
-
         <Route exact path="/enquire" element={<Enquire />} />
-        
         <Route exact path="/products" element={<Products/>} />
-
+        <Route exact path="/footer" element={<Footer/>} />
 
       </Routes>
     </Router>
